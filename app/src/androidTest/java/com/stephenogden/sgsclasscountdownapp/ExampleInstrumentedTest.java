53ae4e2fe6881384db7209cbe7ca7858d437678a
Stephen <jeffrypig23@gmail.com>
2018-03-16 17:31:08 -0700

    Had to rebuild the thing, rip

--- app/src/androidTest/java/com/stephenogden/sgsclasscountdownapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/stephenogden/sgsclasscountdownapp/ExampleInstrumentedTest.java
@@ -21,6 +21,6 @@ public class ExampleInstrumentedTest {
         // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
-        assertEquals("com.stephenogden.classcountdownspanish", appContext.getPackageName());
+        assertEquals("com.stephenogden.sgsclasscountdownapp", appContext.getPackageName());
     }
 }
---
+++ app/src/main/java/com/stephenogden/sgsclasscountdownapp/root.java
@@ -0,0 +1,13 @@
+package com.stephenogden.sgsclasscountdownapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class root extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.root);
+    }
+}
