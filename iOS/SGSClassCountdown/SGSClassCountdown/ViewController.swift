99917ac7c04444fa021b0caa285d5b022ae46318
Stephen <jeffrypig23@gmail.com>
2018-04-13 20:45:14 -0700

    iOS? iOS!

---
+++ iOS/SGSClassCountdown/SGSClassCountdown/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SGSClassCountdown
+//
+//  Created by Stephen Ogden on 4/13/18.
+//  Copyright © 2018 Spud. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
