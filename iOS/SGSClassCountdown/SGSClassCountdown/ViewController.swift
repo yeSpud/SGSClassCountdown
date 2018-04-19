d3cc2934b76d5a7592af1ce0345624cc03ba56ef
Stephen <jeffrypig23@gmail.com>
2018-04-19 15:02:52 -0700

    Updated icons

--- iOS/SGSClassCountdown/SGSClassCountdown/ViewController.swift
+++ iOS/SGSClassCountdown/SGSClassCountdown/ViewController.swift
@@ -43,35 +43,35 @@ class ViewController: UIViewController {
             block.text?.append(" block")
             if (fooTime.getWeekday().lowercased() == "monday" || fooTime.getWeekday().lowercased() == "tuesday" || fooTime.getWeekday().lowercased() == "friday") {
                 // 8 period day
-                if (fooTime.getBlock().lowercased().contains("a")) {
+                if (fooTime.getBlock().lowercased().contains("a -")) {
                     // A block
                     endTime.text = "9:00"
                     countdown.text = fooTime.timeRemaining(hour: 9, minute: 0)
-                } else if (fooTime.getBlock().lowercased().contains("b")) {
+                } else if (fooTime.getBlock().lowercased().contains("b -")) {
                     // B block
                     endTime.text = "9:45"
                     countdown.text = fooTime.timeRemaining(hour: 9, minute: 45)
-                } else if (fooTime.getBlock().lowercased().contains("c")) {
+                } else if (fooTime.getBlock().lowercased().contains("c -")) {
                     // C block
                     endTime.text = "10:45"
                     countdown.text = fooTime.timeRemaining(hour: 10, minute: 45)
-                } else if (fooTime.getBlock().lowercased().contains("d")) {
+                } else if (fooTime.getBlock().lowercased().contains("d -")) {
                     // D block
                     endTime.text = "11:30"
                     countdown.text = fooTime.timeRemaining(hour: 11, minute: 30)
-                } else if (fooTime.getBlock().lowercased().contains("e")) {
+                } else if (fooTime.getBlock().lowercased().contains("e -")) {
                     // E block
                     endTime.text = "12:15"
                     countdown.text = fooTime.timeRemaining(hour: 12, minute: 15)
-                } else if (fooTime.getBlock().lowercased().contains("f")) {
+                } else if (fooTime.getBlock().lowercased().contains("f -")) {
                     // F block
                     endTime.text = "1:40"
                     countdown.text = fooTime.timeRemaining(hour: 13, minute: 40)
-                } else if (fooTime.getBlock().lowercased().contains("g")) {
+                } else if (fooTime.getBlock().lowercased().contains("g -")) {
                     // G block
                     endTime.text = "2:25"
                     countdown.text = fooTime.timeRemaining(hour: 14, minute: 25)
-                } else if (fooTime.getBlock().lowercased().contains("h")) {
+                } else if (fooTime.getBlock().lowercased().contains("h -")) {
                     // H block
                     endTime.text = "3:10"
                     countdown.text = fooTime.timeRemaining(hour: 15, minute: 10)
@@ -81,16 +81,17 @@ class ViewController: UIViewController {
                 }
             } else if (fooTime.getWeekday().lowercased() == "wednesday" || fooTime.getWeekday().lowercased() == "thursday") {
                 // Long block days
-                if (fooTime.getBlock().lowercased().contains("a") || fooTime.getBlock().lowercased().contains("e")) {
+                
+                if (fooTime.getBlock().lowercased().contains("a -") || fooTime.getBlock().lowercased().contains("e -")) {
                     endTime.text = "9:45"
                     countdown.text = fooTime.timeRemaining(hour: 9, minute: 45)
-                } else if (fooTime.getBlock().lowercased().contains("b") || fooTime.getBlock().lowercased().contains("f")) {
+                } else if (fooTime.getBlock().lowercased().contains("b -") || fooTime.getBlock().lowercased().contains("f -")) {
                     endTime.text = "11:25"
                     countdown.text = fooTime.timeRemaining(hour: 11, minute: 25)
-                } else if (fooTime.getBlock().lowercased().contains("c") || fooTime.getBlock().lowercased().contains("g")) {
+                } else if (fooTime.getBlock().lowercased().contains("c -") || fooTime.getBlock().lowercased().contains("g -")) {
                     endTime.text = "1:30"
                     countdown.text = fooTime.timeRemaining(hour: 13, minute: 30)
-                } else if (fooTime.getBlock().lowercased().contains("d") || fooTime.getBlock().lowercased().contains("h")) {
+                } else if (fooTime.getBlock().lowercased().contains("d -") || fooTime.getBlock().lowercased().contains("h -")) {
                     endTime.text = "3:10"
                     countdown.text = fooTime.timeRemaining(hour: 15, minute: 10)
                 } else {
