56ba9af1aff5e7a65efabde856d5268295cd4b41
Spud <jeffrypig23@gmail.com>
2019-04-08 12:04:10 -0700

    Created a potential template for the regime layout

---
+++ AndroidApp/src/main/java/com/spud/sgsclasscountdownapp/Layout/RegimeLayout.java
@@ -0,0 +1,30 @@
+package com.spud.sgsclasscountdownapp.Layout;
+
+import android.content.Context;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+/**
+ * Created by Stephen Ogden on 4/8/19.
+ */
+public class RegimeLayout extends LinearLayout {
+
+	public TextView title, classCount;
+
+	public Button edit, delete;
+
+	public RegimeLayout(Context context) {
+		super(context);
+
+		LinearLayout root = (LinearLayout) this.getRootView();
+
+		this.title = (TextView) root.getChildAt(0);
+		this.classCount = (TextView) root.getChildAt(1);
+		this.edit = (Button) root.getChildAt(2);
+		this.delete = (Button) root.getChildAt(3);
+
+	}
+
+
+}
